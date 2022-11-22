407c8f0a78af52bbeaa7ef3c3c54c68363741757
Tomasz Grabowski <tomasz.grabowski@ihsmarkit.com>
2022-11-22 14:48:31 +0100

    wip

---
+++ dist/cjs/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.Button = void 0;
+var Button_1 = require("./components/button/Button");
+exports.Button = Button_1.Button;
+//# sourceMappingURL=index.js.map
